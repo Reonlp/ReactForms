3571832102911983dd2d69d9dc3aca2a2b9c00be
Sergio Leon <leonsergio0011@gmail.com>
2019-07-17 18:25:09 +0200

    Introduction to react forms

---
+++ forms-intro/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import MultipleForm from './MultipleForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <MultipleForm />
+    </div>
+  );
+}
+
+export default App;
