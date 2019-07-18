832f8215fdc87c267b938809002767fc5d79fe25
Sergio Leon <leonsergio0011@gmail.com>
2019-07-18 06:50:41 +0200

    Added a complete example of a form where the data is passed from the children to the State

--- forms-intro/src/App.js
+++ forms-intro/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './Form';
 import MultipleForm from './MultipleForm';
+import ShoppingList from './ShoppingList';
 
 function App() {
   return (
     <div className="App">
       <Form />
       <MultipleForm />
+      <ShoppingList />
     </div>
   );
 }
